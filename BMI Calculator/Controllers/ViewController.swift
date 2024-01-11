3708224ca6c76653b5670011d44d8d289c6207fb
Priyal PORWAL <26priyalmrbd@gmail.com>
2024-01-11 15:04:35 +0530

    Add SecondViewController for testing

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weight: UILabel!
+
+
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func heightSliderDidChange(_ sender: UISlider) {
+        print(String(format: "Height: %.1f", sender.value))
+        heightLabel.text = String(format: "%.1fm", sender.value)
+    }
 
+    @IBAction func weightSliderDidChange(_ sender: UISlider) {
+//        print(String(format: "Width: %.0f", sender.value))
+        print("Width: ", Int(sender.value))
+        weight.text = "\(Int(sender.value))Kg"
+    }
+
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let weight = weightSlider.value
+        let height = heightSlider.value
+//        print(weight/(height*height))
+        let bmiValue = weight/pow(height, 2)
+        print(bmiValue)
+
+        let secondViewController = SecondViewController(bmiValue: String(format: "%.1f", bmiValue))
+
+        self.present(secondViewController, animated: true) {
+            
+        }
+    }
 }
-
